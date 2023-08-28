af0c8abe2ec39356102e8c2d85ef8c237dda4a93
Jeffery Harrell <jefferyharrell@gmail.com>
2023-08-28 15:37:35 -0700

    Add preview image and Steam description

--- .github/setup-repo.sh
+++ .github/setup-repo.sh
@@ -8,7 +8,7 @@ gh variable set MOD_NAME -b $MOD_NAME
 gh variable set SLN_PATH -b "Source/Assemblies/$MOD_NAME.sln"
 gh variable set CSPROJ_PATH -b "Source/Assemblies/$MOD_NAME/$MOD_NAME.csproj"
 gh variable set ASSEMBLY_PATH -b "1.4/Assemblies/$MOD_NAME.dll"
-gh variable set ZIP_CONTENTS -b "1.4/ About/ LICENSE"
+gh variable set ZIP_CONTENTS -b "1.4/ About/ CHANGELOG.txt LICENSE"
 
 # Pull request type labels
 
