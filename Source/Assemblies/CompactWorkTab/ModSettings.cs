9f816c7dc8533adbe5c691e9cfe4794b8248323d
Jeffery Harrell <jefferyharrell@gmail.com>
2023-09-03 16:13:39 -0700

    Add option to draw labels at a 60° angle
    - Added new boolean variable `DrawInclinedLabels` to `ModSettings.cs`
    - Exposed `DrawInclinedLabels` using `Scribe_Values.Look`
    - Added checkbox for "Draw labels at a 60° angle" in the settings menu
    - Displayed experimental feature warning message with bug reporting link

--- Source/Assemblies/CompactWorkTab/ModSettings.cs
+++ Source/Assemblies/CompactWorkTab/ModSettings.cs
@@ -7,11 +7,13 @@ namespace CompactWorkTab
     {
         public static bool UseScrollWheel = true;
         public static bool DrawLabelsVertically = true;
+        public static bool DrawInclinedLabels = false;
 
         public override void ExposeData()
         {
             Scribe_Values.Look(ref UseScrollWheel, "UseScrollWheel", true);
             Scribe_Values.Look(ref DrawLabelsVertically, "DrawLabelsVertically", true);
+            Scribe_Values.Look(ref DrawInclinedLabels, "DrawInclinedLabels", false);
 
             base.ExposeData();
         }
@@ -22,6 +24,15 @@ namespace CompactWorkTab
             listing.Begin(inRect);
             listing.CheckboxLabeled("Draw column labels vertically", ref DrawLabelsVertically);
             listing.CheckboxLabeled("Use scroll wheel to change work priorities", ref UseScrollWheel);
+
+            listing.CheckboxLabeled("Draw labels at a 60° angle", ref DrawInclinedLabels);
+
+            Text.Font = GameFont.Tiny;
+            GUI.color = Color.gray;
+            listing.Label("This is an EXPERIMENTAL feature. Please report bugs at https://dsc.gg/CaptainArbitrary.");
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
             listing.End();
         }
     }
