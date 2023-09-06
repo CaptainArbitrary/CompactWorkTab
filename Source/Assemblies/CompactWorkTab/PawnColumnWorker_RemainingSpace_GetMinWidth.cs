afccc9afd64f35bffa06a6a9e9ed8bc1d4ce6274
Jeffery Harrell <jefferyharrell@gmail.com>
2023-09-06 11:16:24 -0700

    Add new file for patching PawnColumnWorker_RemainingSpace.GetMinWidth()
    - Add new file `PawnColumnWorker_RemainingSpace_GetMinWidth.cs`
    - Implement Harmony patch for `PawnColumnWorker_RemainingSpace.GetMinWidth()`
    - Add Prefix method to modify the behavior of the original method
    - Check if ModSettings.DrawLabelsVertically is false, return true if it is
    - Set __result to half of Cache.MinHeaderHeight
    - Return false at the end of the Prefix method

---
+++ Source/Assemblies/CompactWorkTab/PawnColumnWorker_RemainingSpace_GetMinWidth.cs
@@ -0,0 +1,20 @@
+using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace CompactWorkTab
+{
+    [HarmonyPatch(typeof(PawnColumnWorker_RemainingSpace), nameof(PawnColumnWorker_RemainingSpace.GetMinWidth))]
+    public class PawnColumnWorker_RemainingSpace_GetMinWidth
+    {
+        private static bool Prefix(PawnTable table, PawnColumnWorker_RemainingSpace __instance, ref int __result)
+        {
+            if (!ModSettings.DrawLabelsVertically) return true;
+
+            __result = Cache.MinHeaderHeight / 2;
+
+            return false;
+        }
+    }
+}
