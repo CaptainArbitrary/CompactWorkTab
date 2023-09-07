4c586668e70f6fa62911793a7d8820b67f90dd18
Jeffery Harrell <jefferyharrell@gmail.com>
2023-09-07 15:09:39 -0700

    Refactor ModSettings.DoSettingsWindowContents to reduce repetition
    - Created a new private helper method called DoRadioButtonAndTexture
    - Got rid of most of the repeated code
    - Okay I'm lying, GPT4 actually did this for me

--- Source/Assemblies/CompactWorkTab/ModSettings.cs
+++ Source/Assemblies/CompactWorkTab/ModSettings.cs
@@ -23,80 +23,47 @@ namespace CompactWorkTab
             Rect rightColumn = new Rect(inRect) { width = inRect.width / 3f, x = middleColumn.xMax };
 
             Rect firstRow = new Rect(inRect) { height = GenUI.ListSpacing };
-
             Widgets.CheckboxLabeled(firstRow, "Use scroll wheel to change work priorities:", ref UseScrollWheel);
 
             Rect secondRow = new Rect(inRect) { y = firstRow.yMax, height = GenUI.ListSpacing };
+            // Second row intentionally left blank
 
             Rect thirdRow = new Rect(inRect) { y = secondRow.yMax, height = GenUI.ListSpacing };
+            DoRadioButtonAndTexture(leftColumn, thirdRow, "Inclined Headers", Textures.InclinedTexture, HeaderOrientation.Inclined);
+            DoRadioButtonAndTexture(middleColumn, thirdRow, "Vertical Headers", Textures.VerticalTexture, HeaderOrientation.Vertical);
+            DoRadioButtonAndTexture(rightColumn, thirdRow, "Horizontal Headers", Textures.HorizontalTexture, HeaderOrientation.Horizontal);
+        }
 
-            string leftRadioButtonLabel = "Inclined Headers";
-            Vector2 leftRadioButtonLabelSize = Text.CalcSize(leftRadioButtonLabel);
-            Rect leftRadioButtonRect = new Rect(leftColumn.x, thirdRow.y, leftColumn.width, thirdRow.height);
-            leftRadioButtonRect.width = leftRadioButtonLabelSize.x + Widgets.RadioButOnTex.width;
-            leftRadioButtonRect.x = leftColumn.center.x - leftRadioButtonRect.width / 2f;
-            if (Widgets.RadioButtonLabeled(leftRadioButtonRect,"Inclined Headers", HeaderOrientation == HeaderOrientation.Inclined)) HeaderOrientation = HeaderOrientation.Inclined;
-
-            string middleRadioButtonLabel = "Vertical Headers";
-            Vector2 middleRadioButtonLabelSize = Text.CalcSize(middleRadioButtonLabel);
-            Rect middleRadioButtonRect = new Rect(middleColumn.x, thirdRow.y, middleColumn.width, thirdRow.height);
-            middleRadioButtonRect.width = middleRadioButtonLabelSize.x + Widgets.RadioButOnTex.width;
-            middleRadioButtonRect.x = middleColumn.center.x - middleRadioButtonRect.width / 2f;
-            if (Widgets.RadioButtonLabeled(middleRadioButtonRect,"Vertical Headers", HeaderOrientation == HeaderOrientation.Vertical)) HeaderOrientation = HeaderOrientation.Vertical;
-
-            string rightRadioButtonLabel = "Horizontal Headers";
-            Vector2 rightRadioButtonLabelSize = Text.CalcSize(rightRadioButtonLabel);
-            Rect rightRadioButtonRect = new Rect(rightColumn.x, thirdRow.y, rightColumn.width, thirdRow.height);
-            rightRadioButtonRect.width = rightRadioButtonLabelSize.x + Widgets.RadioButOnTex.width;
-            rightRadioButtonRect.x = rightColumn.center.x - rightRadioButtonRect.width / 2f;
-            if (Widgets.RadioButtonLabeled(rightRadioButtonRect,"Horizontal Headers", HeaderOrientation == HeaderOrientation.Horizontal)) HeaderOrientation = HeaderOrientation.Horizontal;
-
-            Rect fourthRow = new Rect(inRect) { y = thirdRow.yMax, height = Textures.InclinedTexture.height + GenUI.Gap * 2f};
-
-            Rect leftPictureRect = new Rect(fourthRow) { width = inRect.width / 3f };
-            Rect leftTexRect = new Rect(leftPictureRect)
+        private void DoRadioButtonAndTexture(Rect column, Rect row, string label, Texture texture, HeaderOrientation orientation)
+        {
+            // Radio Button
+            Vector2 labelSize = Text.CalcSize(label);
+            Rect radioButtonRect = new Rect(column.x, row.y, column.width, row.height)
             {
-                width = Textures.InclinedTexture.width,
-                height = Textures.InclinedTexture.height,
-                center = leftPictureRect.center
+                width = labelSize.x + Widgets.RadioButOnTex.width,
+                x = column.center.x - (labelSize.x + Widgets.RadioButOnTex.width) / 2f
             };
-            if (Event.current.type == EventType.MouseDown && leftTexRect.Contains(Event.current.mousePosition))
+            if (Widgets.RadioButtonLabeled(radioButtonRect, label, HeaderOrientation == orientation))
+                HeaderOrientation = orientation;
+
+            // Texture
+            Rect textureRow = new Rect(column) { y = row.yMax, height = texture.height + GenUI.Gap * 2f };
+            Rect textureRect = new Rect(textureRow)
             {
-                HeaderOrientation = HeaderOrientation.Inclined;
+                width = texture.width,
+                height = texture.height,
+                center = textureRow.center
+            };
+            if (Event.current.type == EventType.MouseDown && textureRect.Contains(Event.current.mousePosition))
+            {
+                HeaderOrientation = orientation;
                 Event.current.Use();
             }
-            GUI.DrawTexture(leftTexRect, Textures.InclinedTexture, ScaleMode.ScaleToFit);
-            if (HeaderOrientation == HeaderOrientation.Inclined) Widgets.DrawBox(leftTexRect);
+            GUI.DrawTexture(textureRect, texture, ScaleMode.ScaleToFit);
 
-            Rect middlePictureRect = new Rect(fourthRow) { width = inRect.width / 3f, x = leftPictureRect.xMax };
-            Rect middleTexRect = new Rect(middlePictureRect)
-            {
-                width = Textures.VerticalTexture.width,
-                height = Textures.VerticalTexture.height,
-                center = middlePictureRect.center
-            };
-            if (Event.current.type == EventType.MouseDown && middleTexRect.Contains(Event.current.mousePosition))
-            {
-                HeaderOrientation = HeaderOrientation.Vertical;
-                Event.current.Use();
-            }
-            GUI.DrawTexture(middleTexRect, Textures.VerticalTexture, ScaleMode.ScaleToFit);
-            if (HeaderOrientation == HeaderOrientation.Vertical) Widgets.DrawBox(middleTexRect);
-
-            Rect rightPictureRect = new Rect(fourthRow) { width = inRect.width / 3f, x = middlePictureRect.xMax };
-            Rect rightTexRect = new Rect(rightPictureRect)
-            {
-                width = Textures.HorizontalTexture.width,
-                height = Textures.HorizontalTexture.height,
-                center = rightPictureRect.center
-            };
-            if (Event.current.type == EventType.MouseDown && rightTexRect.Contains(Event.current.mousePosition))
-            {
-                HeaderOrientation = HeaderOrientation.Horizontal;
-                Event.current.Use();
-            }
-            GUI.DrawTexture(rightTexRect, Textures.HorizontalTexture, ScaleMode.ScaleToFit);
-            if (HeaderOrientation == HeaderOrientation.Horizontal) Widgets.DrawBox(rightTexRect);
+            // Draw selection box if this orientation is selected
+            if (HeaderOrientation == orientation)
+                Widgets.DrawBox(textureRect);
         }
     }
 }
