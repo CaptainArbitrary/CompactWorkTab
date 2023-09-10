63a37f5ea437b8389acd32b6b5fc3697d9f0f9b5
Jeffery Harrell <jefferyharrell@gmail.com>
2023-09-10 08:36:28 -0700

    Embetter the WorkManager class
    - Replaced bool? field with a readonly bool set in a static constructor
    - Used an expression-bodied property for RectYOffset for to make more clean and nice
    - Moved the top-of-file comment down to where it's relevant

--- Source/Assemblies/CompactWorkTab/Mods/WorkManager.cs
+++ Source/Assemblies/CompactWorkTab/Mods/WorkManager.cs
@@ -1,23 +1,22 @@
 ﻿using Verse;
 
-// Unfortunately WorkManager hard-codes its icon size as a private const int = 16, so we also have to hard-code it. See:
-// https://github.com/LordKuper/rimworld-workmanager/blob/9f70c6d8824527f20bd00371b48153c7a5968781/Source/Patches/PawnColumnWorkerWorkPriorityPatch.cs#L21
-
 namespace CompactWorkTab.Mods
 {
     internal static class WorkManager
     {
         private const string PackageId = "LordKuper.WorkManager";
-        private static bool? _modIsActive;
+
+        // Unfortunately WorkManager hard-codes its icon size as a private const int = 16, so we also have to hard-code
+        // it. See (deep breath) https://github.com/LordKuper/rimworld-workmanager/blob/9f70c6d8824527f20bd00371b48153c7a5968781/Source/Patches/PawnColumnWorkerWorkPriorityPatch.cs#L21
         private const int HeaderIconSize = 16 + (int)GenUI.GapTiny;
 
-        public static int RectYOffset
+        private static readonly bool ModIsActive;
+
+        static WorkManager()
         {
-            get
-            {
-                if (_modIsActive == null) _modIsActive = ModsConfig.IsActive(PackageId);
-                return _modIsActive.Value ? HeaderIconSize : 0;
-            }
+            ModIsActive = ModsConfig.IsActive(PackageId);
         }
+
+        public static int RectYOffset => ModIsActive ? HeaderIconSize : 0;
     }
 }
